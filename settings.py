254927edfed686a6bddcc7fd11494d0ed69a96e4
HoJenson <hojenson@163.com>
2024-01-24 15:52:57 +0800

    #

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 UNK = 0                         # 未登录词标识符的索引
 PAD = 1                         # padding占位符的索引
 
-BATCH_SIZE = 32                 # 批次大小
+BATCH_SIZE = 64                 # 批次大小
 EPOCHS = 100                    # 训练轮数
 LAYERS = 3                      # transformer中encoder和decoder层数
 H_NUM = 4                       # multi head attention hidden个数
